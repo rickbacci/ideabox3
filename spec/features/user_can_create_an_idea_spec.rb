57ddcf7f6ca2ba724b455ee7cd0a4ec55d9c27d5
Rick Bacci <rickbacci@gmail.com>
2015-11-30 15:08:49 -0700

    add feature spec for idea creation

---
+++ spec/features/user_can_create_an_idea_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.feature "Idea interactions", :type => :feature do
+
+  scenario "User creates a new idea" do
+
+    visit root_path
+
+    fill_in 'title', with: 'New Idea'
+    fill_in 'body', with: 'New idea body'
+
+    click_on "Create Idea"
+
+    expect(page).to have_content('New Idea')
+    expect(page).to have_content('New idea body')
+  end
+end
