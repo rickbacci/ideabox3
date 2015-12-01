70241b2489d54d308ab36bd446509c089fd8c9ef
Rick Bacci <rickbacci@gmail.com>
2015-12-01 03:04:19 -0700

    add delete button and ajax

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      get '/ideas', to: 'ideas#index'
-      post '/ideas', to: 'ideas#create'
+      get    '/ideas',     to: 'ideas#index'
+      post   '/ideas',     to: 'ideas#create'
+      delete '/ideas/:id', to: 'ideas#destroy'
     end
   end
   root to: 'api/v1/ideas#index'
