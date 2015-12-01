72bd401ddf839af6128b75707014e3179efa715a
Rick Bacci <rickbacci@gmail.com>
2015-12-01 03:01:42 -0700

    add delete idea feature spec

--- spec/features/user_can_delete_an_idea_spec.rb
+++ spec/features/user_can_delete_an_idea_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 RSpec.feature "Idea deletion", :type => :feature do
 
-
   scenario "User can delete an idea", js: true do
 
     visit root_path
@@ -10,8 +9,12 @@ RSpec.feature "Idea deletion", :type => :feature do
     fill_in 'title', with: 'New Idea'
     fill_in 'body', with: 'New idea body'
 
-    click_on "Create Idea"
+    click_on 'Create Idea'
 
     expect(page).to have_content('delete')
+
+    click_on 'delete'
+
+    expect(page).to_not have_content('delete')
   end
 end
