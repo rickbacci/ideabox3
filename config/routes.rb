5b78189eb4e028071bd257f80f33753802c0c824
Rick Bacci <rickbacci@gmail.com>
2015-11-30 23:06:47 -0700

    add idea routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  root 'ideas#index'
 
-
+  namespace :api do
+    namespace :v1 do
+      get '/ideas', to: 'ideas#index'
+      post '/ideas', to: 'ideas#create'
+    end
+  end
+  root to: 'api/v1/ideas#index'
 end
