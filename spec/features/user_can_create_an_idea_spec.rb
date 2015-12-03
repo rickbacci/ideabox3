b55409559c0c79955403bc54f9341f3e00fb7ca6
Rick Bacci <rickbacci@gmail.com>
2015-12-03 00:20:58 -0700

    add truncate body spec

--- spec/features/user_can_create_an_idea_spec.rb
+++ spec/features/user_can_create_an_idea_spec.rb
@@ -35,4 +35,34 @@ RSpec.feature "Idea creation", :type => :feature do
     expect(page).to have_content('New Idea')
     expect(page).to have_content('New idea body')
   end
+
+  scenario "Idea body not truncated", js: true do
+
+    visit root_path
+
+    fill_in 'title', with: 'New Idea'
+    fill_in 'body', with: 'New idea body' * 7
+
+    click_on "Create Idea"
+
+    expect(page).to have_content('New Idea')
+
+    expect(page).to_not have_content('...')
+  end
+
+  scenario "Idea body is truncated", js: true do
+
+    body = 'New idea body' * 8
+
+    visit root_path
+
+    fill_in 'title', with: 'New Idea'
+    fill_in 'body', with: body
+
+    click_on "Create Idea"
+
+    expect(page).to have_content('New Idea')
+
+    expect(page).to have_content('idea...')
+  end
 end
